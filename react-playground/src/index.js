11a92cb1d2d394ce14c45b55b20c0e0bcc846063
Christina Chapman <cmchapman75@gmail.com>
2019-11-13 12:28:24 -0600

    worked through handleClick

--- react-playground/src/index.js
+++ react-playground/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom'; 
-import App from './App'; 
 import './index.css';
+import HelloWorld from './state-drills/HelloWorld'
 
-ReactDOM.render(<App />, 
+ReactDOM.render(<HelloWorld />, 
     document.getElementById('root'));
 
---
+++ react-playground/src/state-drills/HelloWorld.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+    state = {
+        who: 'world'
+    }
+
+
+    handleClick = (e) => {
+        this.setState({
+             who: e.target.innerText
+             
+          
+        });    
+    }
+    render()  {
+      return (
+        <div>
+            <p>Hello, {this.state.who} !</p>
+            <button onClick={() =>this.handleClick}>Friend!</button>
+            <button onClick={this.handleClick}>World</button>
+            <button onClick={this.handleClick}>React</button>
+        </div>
+    )
+  }
+}
+
+export default HelloWorld;
+
+// onClick={() => this.handleClick('World')}
+
+// word
