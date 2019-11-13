0c8db7d5643827516538589e9dc0d470bee545db
Christina Chapman <cmchapman75@gmail.com>
2019-11-13 15:44:10 -0600

    worked through RouletteGun

--- react-playground/src/index.js
+++ react-playground/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom'; 
 import './index.css';
 // import HelloWorld from './state-drills/HelloWorld'
-import Bomb from './state-drills/BombDrill'
+// import Bomb from './state-drills/BombDrill'
+import RouletteGun from './state-drills/RouletteGun'
 
 
-ReactDOM.render(<Bomb />, 
+ReactDOM.render(<RouletteGun />, 
     document.getElementById('root'));
 
